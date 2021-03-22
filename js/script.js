3cd341edbdaffd89cea22c31bf24f510a7c8efae
Danny <danny.brogno@gmail.com>
2021-03-22 18:06:47 +0100

    End of project

--- js/script.js
+++ js/script.js
@@ -2,19 +2,38 @@
 function previousClick() {
 
   var currentImg = $(".active");
-  var previousImg = currentImg.prev("img");
 
-  currentImg.removeClass("active");
-  previousImg.addClass("active");
+  if (currentImg.hasClass("start")) {
+
+    var previousImg = $("img.end");
+    currentImg.removeClass("active");
+    previousImg.addClass("active");
+
+  } else {
+
+    var previousImg = currentImg.prev("img");
+    currentImg.removeClass("active");
+    previousImg.addClass("active");
+  }
+
 }
 
 function nextClick() {
 
   var currentImg = $(".active");
-  var nextImg = currentImg.next("img");
 
-  currentImg.removeClass("active");
-  nextImg.addClass("active");
+  if (currentImg.hasClass("end")) {
+
+    var nextImg = $("img.start");
+    currentImg.removeClass("active");
+    nextImg.addClass("active");
+
+  } else {
+
+    var nextImg = currentImg.next("img");
+    currentImg.removeClass("active");
+    nextImg.addClass("active");
+  }
 }
 
 function init () {
