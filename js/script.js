dfb2f5b2670b30e1aaf04328ab444c8abb4fa09b
Danny <danny.brogno@gmail.com>
2021-03-22 17:49:42 +0100

    active

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-// 
-// function init () {
-//   $("#arrow-right").click(nextPicture);
-//   $("#arrow-left").click(previousPicture);
-// }
-//
-// $(document).ready(init);
+
+function previousClick() {
+
+  var currentImg = $(".active");
+  var previousImg = currentImg.prev("img");
+
+  currentImg.removeClass("active");
+  previousImg.addClass("active");
+}
+
+function nextClick() {
+
+  var currentImg = $(".active");
+  var nextImg = currentImg.next("img");
+
+  currentImg.removeClass("active");
+  nextImg.addClass("active");
+}
+
+function init () {
+  $(".right-arrow").click(nextClick);
+  $(".left-arrow").click(previousClick);
+}
+
+$(document).ready(init);
